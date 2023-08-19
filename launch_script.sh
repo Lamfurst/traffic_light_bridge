3d52e6de5719e5708de0e3ec6571c5a01341c3dd
Lamfurst <lamfurst@outlook.com>
2023-08-19 18:08:28 -0400

    Update launch

--- launch_script.sh
+++ launch_script.sh
@@ -1,3 +1,20 @@
+# #!/bin/bash
+# # Parse command-line argument
+# # Check whethe to pub all traffic light info
+# PUB_ALL=$1
+
+# # launch reroute node in a new terminal
+# tmux split-window -h
+# tmux send-keys "source /home/artemis/Workspace/autowarefoundation/carla-autoware-universe/autoware/install/setup.bash" C-m
+# tmux send-keys "source /home/artemis/personal_autoware_ws/install/setup.bash" C-m
+
+# if [ "$PUB_ALL" == "false" ]; then
+#     tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml pub_all:=false" C-m
+# else
+#     tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml" C-m
+# fi
+
+
 #!/bin/bash
 # Parse command-line argument
 # Check whethe to pub all traffic light info
@@ -5,11 +22,10 @@ PUB_ALL=$1
 
 # launch reroute node in a new terminal
 tmux split-window -h
-tmux send-keys "source /home/artemis/Workspace/autowarefoundation/carla-autoware-universe/autoware/install/setup.bash" C-m
-tmux send-keys "source /home/artemis/personal_autoware_ws/install/setup.bash" C-m
+tmux send-keys "source ${HOMEDIR}/sim_helper_ws/install/setup.bash" C-m
 
 if [ "$PUB_ALL" == "false" ]; then
-    tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml pub_all:=false" C-m
+    tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml carla_host:=172.17.01 pub_all:=false" C-m
 else
-    tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml" C-m
+    tmux send-keys "ros2 launch launch/trafficlightbridge.launch.xml carla_host:=172.17.01" C-m
 fi
